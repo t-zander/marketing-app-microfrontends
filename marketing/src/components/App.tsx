4acba4013723d002c967a67802300c6fb4b161a0
t-zander <alex.sergeevich.tarasenko@gmail.com>
2021-04-21 22:16:16 +0300

    added header

--- marketing/src/components/App.tsx
+++ marketing/src/components/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Landing from "./Landing";
-import Pricing from "./Pricing";
-import { StylesProvider } from "@material-ui/core";
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Landing from './Landing';
+import Pricing from './Pricing';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+// prefixes all classes for marketing app with 'container' instead of jss
+// by default it will create smth like jss-1
+// which will lead to classes collision
+// because our container app uses copy of css in js from material ui
+// and will create the same jss-1 prefix
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'marketing'
+});
 
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
